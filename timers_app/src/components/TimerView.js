dc4335ffe2e1c97dfe40a934a8a411026e96d360
Gideon Crawley <gscrawley@gmail.com>
2021-06-04 06:35:59 -0700

    timers now running perfectly, just gotta make it look cool

--- timers_app/src/components/TimerView.js
+++ timers_app/src/components/TimerView.js
@@ -2,6 +2,8 @@ import React, {  } from 'react'
 import { useDispatch } from 'react-redux'
 // Import our toggleTimer action
 import { toggleTimer } from '../actions'
+import { formatTime } from '../utils';
+
 import './TimerView.css'
 
 export default function TimerView(props) {
@@ -12,7 +14,8 @@ export default function TimerView(props) {
   return (
     <div className="TimerView">
       <h2>{timer.name}</h2>
-      <h1>{timer.time}</h1>
+      <h1>{formatTime(timer.time)}</h1>
+
       <button
         onClick={() => dispatch(toggleTimer(index))}
       >
