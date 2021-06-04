15d92a8d6f14e25e3cee09e51313bffb7b09adcc
Gideon Crawley <gscrawley@gmail.com>
2021-06-03 21:44:21 -0700

    added Timer class

---
+++ timers_app/src/Timer.js
@@ -0,0 +1,14 @@
+class Timer {
+    // The name property is passed into the constructor and the class is initialized.
+    constructor(name) {
+      this.name = name;
+      this.time = 0;
+      this.isRunning = false;
+    }
+  }
+  
+  export default Timer;
+
+//   Below is an example on how to create an instance of `Timer`:
+// var myTimer = new Timer('Workout');
+
